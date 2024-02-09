4b84f42eaea13296e43777c8c21c41b9ea655c6b
FerMqz94 <fernando.mqz94@email.com>
2024-02-09 15:16:45 -0300

    No recuerdo si hice algun cambio, por las dudas hago un commit

--- controllers/other/main.controller.js
+++ controllers/other/main.controller.js
@@ -1,5 +1,5 @@
 const path = require('path')
 
 module.exports = (req,res) => {     
-    res.sendFile(path.join(__dirname,'../views/home.html'))
+    res.sendFile(path.join(__dirname,'../../views/home.html'))
 }
